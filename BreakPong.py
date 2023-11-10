6814b607b97da27b79a673ed60174d9c5c56376e
Jack Lin <jack.lin11@myhunter.cuny.edu>
2023-11-10 10:31:22 -0500

    I did a thing.

---
+++ BreakPong.py
@@ -0,0 +1,11 @@
+
+#Main code for BreakPong 
+#Additional parts of code will be modified from this
+import pygame 
+pygame.init()
+#fps lock I believe
+fps=60
+fpsClock=pygame.time.Clock()
+size=width, height= 1280, 920
+screen=pygame.display.set_mode(size)
+
