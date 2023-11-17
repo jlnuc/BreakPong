eb1f6714ffda513da1b2a8c153babba71113f855
Jack Lin <jack.lin11@myhunter.cuny.edu>
2023-11-17 11:04:59 -0500

    added some more sprite classes

--- BreakPong.py
+++ BreakPong.py
@@ -3,6 +3,7 @@
 #Additional parts of code will be modified from this
 import pygame 
 from pygame.locals import *
+import random
 pygame.init()
 #fps lock I believe
 fps=60
@@ -39,4 +40,34 @@ class Player2(pygame.sprite.Sprite):
             self.rect.top=0
         elif self.rect.bottom >= height:
             self.rect.bottom=height
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Block, self).__init__()
+        self.surf= #block surface
+        self.rect= self.surf.get_rect(
+                center=(
+                    #positions of the blocks
+                    )
+                )
+    def update(self):
+        if #ball hits block:
+        self.kill()
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Ball, self).__init__()
+        self.surf= #ball surface
+        self.rect=self.surf.get_rect(
+                center=(
+                    #center of the screen
+                    )
+                )
+    def update(self):
+        self.rect.move_(#randX, randY#)
+        if self.rect.right < 0:
+            self.kill()
+        elif self.rect.left > width:
+            self.kill()
+player1=Player1()
+player2=Player2()
+
 
